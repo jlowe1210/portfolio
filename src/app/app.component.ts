302753a76fbb99dad2b7c64aecfbdeecb76a416c
Justin Lowe <justin.lowe53@yahoo.com>
2023-10-04 21:52:59 -0400

    reorder projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,51 +7,32 @@ import Project from './models/Project.model';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor() {
+    Array.from({ length: 20 });
+  }
+
   public projects: Project[] = [
     {
-      name: 'Simple Chat app',
-      description:
-        'real time chat app with both react and angular connecting to the same node backend, so no matter which front end you choose you will able to see the other front end messages',
-      imgSrc: 'assets/chatapp.png',
+      name: 'The Insurance Lady',
+      description: 'web site for insurance broker located in atlanta',
+      imgSrc: 'assets/insuranceladyapp.png',
       links: [
-        'https://simplechatapp.com/angular',
-        'https://simplechatapp.com/react',
+        'https://theinsuranceladyofatlanta.com',
+        'https://insuranceladyreactbackend-production.up.railway.app/',
       ],
       githubLink: [
-        'https://github.com/jlowe1210/chatapp-frontend-angular',
-        'https://github.com/jlowe1210/chat-app-react',
+        'https://github.com/jlowe1210/insurance_lady_angular',
+        'https://github.com/jlowe1210/insurance_lady_react',
       ],
-      techUsed: ['socket io, react, angular, node js, mysql, sequelize'],
-    },
-    {
-      name: 'hangman',
-      description:
-        'simple hangman game, but incase you dont know what hangman is, its a game a player tries to guess the letters of a word, and failed attempts are recorded by drawing a gallows and someone hanging on it, line by line.',
-      imgSrc: 'assets/hangmanapp.png',
-      links: [
-        'https://jlowe1210.github.io/hangmanangular/',
-        'https://hangmanreact.com/',
+      techUsed: [
+        'react',
+        'react-hook-form',
+        'angular',
+        'mysql',
+        'node js',
+        'sequelize',
+        'google maps',
       ],
-      githubLink: [
-        'https://github.com/jlowe1210/hangmanangular',
-        'https://github.com/jlowe1210/hangmanreact',
-      ],
-      techUsed: ['react', 'angular'],
-    },
-    {
-      name: 'weatherapp',
-      description:
-        'weather app, yes i know what you are think another weather app... but i believe my weather app is different. In my app i render the globe and you are able to get the weather from any place you clicked on the globe, you may also type the location too',
-      imgSrc: 'assets/weatherapp.png',
-      links: [
-        'https://jlowe1210.github.io/angular-weather-app-v2/',
-        'https://jlowe1210.github.io/weather_app_react_v2/',
-      ],
-      githubLink: [
-        'https://github.com/jlowe1210/angular-weather-app-v2',
-        'https://github.com/jlowe1210/weather_app_react_v2',
-      ],
-      techUsed: ['MapBox api', 'open weather api', 'react', 'angular'],
     },
     {
       name: 'Online poll App',
@@ -97,26 +78,49 @@ export class AppComponent {
       ],
     },
     {
-      name: 'The Insurance Lady',
-      description: 'web site for insurance broker located in atlanta',
-      imgSrc: 'assets/insuranceladyapp.png',
+      name: 'Simple Chat app',
+      description:
+        'real time chat app with both react and angular connecting to the same node backend, so no matter which front end you choose you will able to see the other front end messages',
+      imgSrc: 'assets/chatapp.png',
       links: [
-        'https://theinsuranceladyofatlanta.com',
-        'https://insuranceladyreactbackend-production.up.railway.app/',
+        'https://simplechatapp.com/angular',
+        'https://simplechatapp.com/react',
       ],
       githubLink: [
-        'https://github.com/jlowe1210/insurance_lady_angular',
-        'https://github.com/jlowe1210/insurance_lady_react',
+        'https://github.com/jlowe1210/chatapp-frontend-angular',
+        'https://github.com/jlowe1210/chat-app-react',
       ],
-      techUsed: [
-        'react',
-        'react-hook-form',
-        'angular',
-        'mysql',
-        'node js',
-        'sequelize',
-        'google maps',
+      techUsed: ['socket io, react, angular, node js, mysql, sequelize'],
+    },
+    {
+      name: 'hangman',
+      description:
+        'simple hangman game, but incase you dont know what hangman is, its a game a player tries to guess the letters of a word, and failed attempts are recorded by drawing a gallows and someone hanging on it, line by line.',
+      imgSrc: 'assets/hangmanapp.png',
+      links: [
+        'https://jlowe1210.github.io/hangmanangular/',
+        'https://hangmanreact.com/',
       ],
+      githubLink: [
+        'https://github.com/jlowe1210/hangmanangular',
+        'https://github.com/jlowe1210/hangmanreact',
+      ],
+      techUsed: ['react', 'angular'],
+    },
+    {
+      name: 'weatherapp',
+      description:
+        'weather app, yes i know what you are think another weather app... but i believe my weather app is different. In my app i render the globe and you are able to get the weather from any place you clicked on the globe, you may also type the location too',
+      imgSrc: 'assets/weatherapp.png',
+      links: [
+        'https://jlowe1210.github.io/angular-weather-app-v2/',
+        'https://jlowe1210.github.io/weather_app_react_v2/',
+      ],
+      githubLink: [
+        'https://github.com/jlowe1210/angular-weather-app-v2',
+        'https://github.com/jlowe1210/weather_app_react_v2',
+      ],
+      techUsed: ['MapBox api', 'open weather api', 'react', 'angular'],
     },
   ];
 }
